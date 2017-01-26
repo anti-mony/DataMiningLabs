2baf6e00b22f2ce6a5bd80425b87dd8a98556957
Sushant Bansal <1star.sushant@gmail.com>
2017-01-26 23:30:13 +0530

    Initial commit

---
+++ src/AssociationRuleMining.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Sushant Bansal, Pragya Chaturvedi, Ishan Tyagi
+ * Association Rule Mining
+ *
+ */
+public class AssociationRuleMining {
+    public static void main(String[] args) {
+        System.out.println("Enter the minimum threshold");
+    }
+}
